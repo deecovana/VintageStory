138ea4d9761e1edffca1f888e7f1c56374de1b11
DeeCovana <deecovana@gmail.com>
2026-04-13 15:09:22 +0300

    Update-origins-13.04.2026-15:09:20 69

--- run1366.clouds.bat
+++
@@ -1,4 +0,0 @@
-C:\Programs\QRes.exe /x:1366 /y:768
-copy clientsettings.json.fix.1366.clouds clientsettings.json
-"C:\Games\VintageStory\PlayVintageStory.exe"
-copy clientsettings.json clientsettings.json.fix.1366.clouds
--- run1440.clouds.bat
+++
@@ -1,4 +0,0 @@
-C:\Programs\QRes.exe /x:1440 /y:900
-copy clientsettings.json.fix.1440.clouds clientsettings.json
-"C:\Games\VintageStory\PlayVintageStory.exe"
-copy clientsettings.json clientsettings.json.fix.1440.clouds
