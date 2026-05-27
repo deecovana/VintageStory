23b3d4439bbd688ac498473ebdf1d66a54698437
DeeCovana <deecovana@gmail.com>
2026-05-27 23:50:46 +0300

    Up: Start default

--- run1280.clouds.bat
+++ run1280.clouds.bat
@@ -1,4 +1,4 @@
-C:\Programs\QRes.exe /x:1280 /y:720
+C:\Programs\QRes.exe /x:1280 /y:800
 copy clientsettings.json.fix.1280.clouds clientsettings.json
 "C:\Games\VintageStory\PlayVintageStory.exe"
 copy clientsettings.json clientsettings.json.fix.1280.clouds
---
+++ run1600.clouds.bat
@@ -0,0 +1,4 @@
+C:\Programs\QRes.exe /x:1600 /y:1000
+copy clientsettings.json.fix.1600.clouds clientsettings.json
+"C:\Games\VintageStory\PlayVintageStory.exe"
+copy clientsettings.json clientsettings.json.fix.1600.clouds
